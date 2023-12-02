9f7a4f325807fac048517bed6fcae7169e9f0bb1
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-12-02 10:06:14 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import CustomerRoutes from "./src/routes/customerRouter.js"
-import Connection from "./src/config/connection.js";
+import Connection from "./src/db/Connection.js";
 
 const port = process.env.PORT || 3000;
 
