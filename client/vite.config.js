4b611ffb4e2abb759d409f929173ef296877617c
SHANKAR JADAV <shankarjadav360@gmail.com>
2024-04-04 20:30:26 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+});
